6ac2dfec19d884d248c14f036c8867c0c62e61ea
Amdounislim <amdouni.slim@gmail.com>
2021-09-27 12:46:45 +0100

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function NavBar() {
+
+    let name="Med"
+
+
+    return (
+        <div>
+            <h1>Hello</h1>
+            <p>I'm {name}</p>
+        </div>
+    )
+}
+
+
+
